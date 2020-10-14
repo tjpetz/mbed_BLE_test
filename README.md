07bfa595acd5675bd701d3543968a91254c134f4
Tom Petz <tom@tjpetz.com>
2020-10-14 16:57:30 -0400

    Fix for public address and a readme.

---
+++ README.md
@@ -0,0 +1,23 @@
+# mbed_BLE_test
+
+This is an extension of the mbedOS battery example.  It includes the environmental service and uses
+the sensor on the Arduino Nano 33 BLE Sense board.
+
+## Public and Random BLE address on macOS
+
+By default the mbedOS library advertises with Random Addresses.  This is different than the default
+for the Arduino BLE library which uses the Public Address to advertise.  This difference caused
+connectivity problems with macOS clients.  An iOS 14 (either iPhone or iPad) could connect but
+the similar client on a macOS 10.15 (Catalina) machine would scan but not connect.  Using the
+PacketLogger application include in the XCode Extra Tools, I was able to observe the different
+between connecting to a public and random address.  Force the mbedOS peripheral to advertise
+with the public address resolved the problem.
+
+```
+    ble_error_t error = _ble.gap().setAdvertisingParameters(
+        ble::LEGACY_ADVERTISING_HANDLE,  
+        ble::AdvertisingParameters()
+          .setOwnAddressType(ble::own_address_type_t::PUBLIC)
+          .setTxPower(8)
+          );
+```
--- mbed_BLE_test.ino
+++ mbed_BLE_test.ino
@@ -3,6 +3,9 @@
 #include "ble/BLE.h"
 #include "ble/Gap.h"
 #include "ble/services/BatteryService.h"
+#include "ble/services/EnvironmentalService.h"
+#include <Arduino_HTS221.h>
+#include <Arduino_LPS22HB.h>
 
 static mbed::DigitalOut led1(LED1, 1);
 
@@ -22,14 +25,17 @@ public:
   BatteryDemo(BLE &ble, events::EventQueue &event_queue)
       : _ble(ble), _event_queue(event_queue),
         _battery_uuid(GattService::UUID_BATTERY_SERVICE), _battery_level(50),
-        _battery_service(ble, _battery_level), _adv_data_builder(_adv_buffer) {}
+        _environmental_uuid(GattService::UUID_ENVIRONMENTAL_SERVICE),
+        _temperature(-40), _humidity(0), _pressure(0),
+        _battery_service(ble, _battery_level), 
+        _environmental_service(ble),
+        _adv_data_builder(_adv_buffer) {}
 
   void start() {
     _ble.gap().setEventHandler(this);
     _ble.init(this, &BatteryDemo::on_init_complete);
     _event_queue.call_every(500, this, &BatteryDemo::blink);
     _event_queue.call_every(1000, this, &BatteryDemo::update_sensor_value);
-    _event_queue.call_every(15000, this, &BatteryDemo::status);
     _event_queue.dispatch_forever();
   }
 
@@ -46,27 +52,19 @@ private:
   }
 
   void start_advertising() {
-    ble::AdvertisingParameters adv_parameters(
-        ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
-        ble::adv_interval_t(ble::millisecond_t(1000)));
 
+    // setup the advertising data, defaults and add the battery service uuid.
     _adv_data_builder.setFlags();
-    _adv_data_builder.setLocalServiceList(mbed::make_Span(&_battery_uuid, 1));
+    _adv_data_builder.setLocalServiceList(mbed::make_Span(&_environmental_uuid, 1));
     _adv_data_builder.setName(DEVICE_NAME);
 
-    adv_parameters.setTxPower(-12);
-
-    // ble_error_t error = _ble.gap().setAdvertisingParameters(
-    //     ble::LEGACY_ADVERTISING_HANDLE, adv_parameters);
+    // Note, we force the Own Address type to public.  The default is random which
+    // seems to prevent connections from macOS 10.15 (and possibly other version).
     ble_error_t error = _ble.gap().setAdvertisingParameters(
         ble::LEGACY_ADVERTISING_HANDLE,  
-        ble::AdvertisingParameters(
-          // ble::advertising_type_t::CONNECTABLE_NON_SCANNABLE_UNDIRECTED,
-          // false
-          ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
-          true
-          )
-            .setTxPower(8)
+        ble::AdvertisingParameters()
+          .setOwnAddressType(ble::own_address_type_t::PUBLIC)
+          .setTxPower(8)
           );
 
     if (error) {
@@ -91,84 +89,53 @@ private:
   }
 
   void update_sensor_value() {
-    if (_ble.gap().getState().connected) {
+   if (_ble.gap().getState().connected) {
       _battery_level++;
       if (_battery_level > 100)
         _battery_level = 20;
 
       _battery_service.updateBatteryLevel(_battery_level);
-    }
+
+      _environmental_service.updateTemperature(HTS.readTemperature() * 100);
+      _environmental_service.updateHumidity(HTS.readHumidity() * 100);
+      _environmental_service.updatePressure(BARO.readPressure() * 10000);
+   }
   }
 
   void blink(void) { led1 = !led1; }
 
-  void status(void) {
-    // static int8_t power = -4;
-    // power += 2;
-    // if (power > 4)
-    //   power = -4;
-
-    // Serial.print("Setting Power to: ");
-    // Serial.println(power);
-    // _ble.gap().setTxPower(power);
-
-    // const int8_t *p;
-    // size_t count;
-
-    // _ble.gap().getPermittedTxPowerValues(&p, &count);
-
-    // if (count > 0) {
-    //   Serial.print("count = ");
-    //   Serial.println(count);
-    //   for (int i = 0; i < count; i++) {
-    //     Serial.print("power = ");
-    //     Serial.println(p[i]);
-    //   }
-    // } else {
-    //   Serial.println("Permitted powers didn't return anything!");
-    // }
-
-    // Gap::ConnectionParams_t connParams;
-    // ble_error_t error = _ble.gap().getPreferredConnectionParams(&connParams);
-    // if (error == BLE_ERROR_NONE) {
-    //   Serial.println("Connection params:");
-    //   Serial.println(connParams.minConnectionInterval);
-    // } else {
-    //   Serial.println("Error getting preferred connection params!");
-    // }
-
-    Serial.println("================================");
-
-    Serial.print("Max AdvertisingSetNumber: ");
-    Serial.println(_ble.gap().getMaxAdvertisingSetNumber());
-    Serial.print("LE_ENCRYPTION: ");
-    Serial.println(_ble.gap().isFeatureSupported(ble::controller_supported_features_t::LE_ENCRYPTION));
-    Serial.print("LE_2M_PHY: ");
-    Serial.println(_ble.gap().isFeatureSupported(ble::controller_supported_features_t::LE_2M_PHY));
-
-  }
-
 private:
   virtual void
-  onDisconnectionComplete(const ble::DisconnectionCompleteEvent &) {
+  onDisconnectionComplete(const ble::DisconnectionCompleteEvent & a) {
     Serial.println("Disconnecting");
     _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
   }
 
-  virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &) {
+  virtual void onConnectionComplete(const ble::ConnectionCompleteEvent & a) {
     Serial.println("Connecting!");
   }
 
-  virtual void onAdvertisingEnd(const ble::AdvertisingEndEvent &) {
+  virtual void onAdvertisingEnd(const ble::AdvertisingEndEvent & a) {
     Serial.println("Restarting advertising");
     _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+  }
+
+  virtual void onUpdateConnectionParametersRequest(const ble::UpdateConnectionParametersRequestEvent & a) {
+    Serial.println("Received request to update connection parameters!\n");
   }
 
   BLE &_ble;
   events::EventQueue &_event_queue;
   UUID _battery_uuid;
+  UUID _environmental_uuid;
   uint8_t _battery_level;
+  int16_t _temperature;
+  uint16_t _humidity;
+  uint32_t _pressure;
+
   BatteryService _battery_service;
+  EnvironmentalService _environmental_service;
+
   uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
   ble::AdvertisingDataBuilder _adv_data_builder;
 };
@@ -179,9 +146,12 @@ void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
 
 void setup() {
   Serial.begin(115200);
-  wait(5);
+  delay(5);
 
   Serial.println("Starting...");
+
+  HTS.begin();
+  BARO.begin();
 
   BLE &ble = BLE::Instance();
   ble.onEventsToProcess(schedule_ble_events);
